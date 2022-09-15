fced34be800e816a6c0a884bb33917a7b62e1936
Marc-hdev <102926163+Mhenrydev@users.noreply.github.com>
2022-09-15 11:13:11 +0200

    maj
    maj

--- controller/creation_element.php
+++ controller/creation_element.php
@@ -4,22 +4,21 @@ $filmsDao = new FilmsDAO();
 
 
 //Si titre ou realisateur est soumis
-if (isset($_POST['titre'])) {
-
+if (isset($_POST['realisateur'])) {
     //Crée une nouveau film 
-    $film = new Films(null, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee'], /*placer le tabRole*/);
-
+    $film = new Films(null, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
+    var_dump($film);
     //Get les elements, construit la requete sql et insère dans la bdd
     $status = $filmsDao->add($film);
 
     //Si $statut est executé alors affiche dans creer_offre.twig le status avec msg 
     if ($status) {
-        echo $twig->render('creer_film.html.twig', ['status' => "Le film à été ajouté", 'film' => $film]);
+        echo $twig->render('creation_element.html.twig', ['status' => "Le film à été ajouté", 'film' => $film]);
     }
     //Sinon alors affiche dans creer_offre.twig le status avec msg erreur  
     else {
-        echo $twig->render('creer_film.html.twig', ['status' => "Erreur Ajout"]);
+        echo $twig->render('creation_element.twig', ['status' => "Erreur Ajout"]);
     }
 } else {
-    echo $twig->render('creer_film.html.twig');
+    echo $twig->render('creation_element.html.twig');
 }
---
+++ controller/gestion_film.php
@@ -0,0 +1,24 @@
+<?php
+//gere connection et les function 
+$filmsDao = new FilmsDAO();
+
+
+//Si titre ou realisateur est soumis
+if (isset($_POST['realisateur'])) {
+    //Crée une nouveau film 
+    $film = new Films(null, $_POST['titre'], $_POST['realisateur'], $_POST['affiche'], $_POST['annee']);
+    var_dump($film);
+    //Get les elements, construit la requete sql et insère dans la bdd
+    $status = $filmsDao->add($film);
+
+    //Si $statut est executé alors affiche dans creer_offre.twig le status avec msg 
+    if ($status) {
+        echo $twig->render('gestion_film.html.twig', ['status' => "Le film à été ajouté", 'film' => $film]);
+    }
+    //Sinon alors affiche dans creer_offre.twig le status avec msg erreur  
+    else {
+        echo $twig->render('gestion_film.twig', ['status' => "Erreur Ajout"]);
+    }
+} else {
+    echo $twig->render('gestion_film.html.twig');
+}
