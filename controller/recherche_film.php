2d45b60fd22b31df92b567e29bc5a2e78f2a7708
Marc-hdev <102926163+Mhenrydev@users.noreply.github.com>
2022-09-16 14:21:12 +0200

    Update rendu final
    Update rendu final

--- controller/recherche_film.php
+++ controller/recherche_film.php
@@ -3,36 +3,33 @@ include('controllerFunctions.php');
 
 authenticate_automatically();
 
-try
-{
+try {
     //gere connection et les function 
-    $filmsDao = new FilmsDAO(); 
+    $filmsDao = new FilmsDAO();
 
     //Si titre ou realisateur est soumis
-    if (isset($_POST['titre'])) {   
+    if (isset($_POST['titre'])) {
         $keywords = make_keywords_title();
-        if (!$keywords)
-        {
+        if (!$keywords) {
             throw new Exception('Element de recherche invalide.');
         }
-        $films = $filmsDao->searchFilm($keywords);   
+        $films = $filmsDao->searchFilm($keywords);
 
         foreach ($films as $film) {
             $acteurList = $filmsDao->filmActeur($film->get_idFilm());
             $film->set_tabRoles($acteurList);
         }
-    
+
         echo $twig->render('recherche_film.html.twig', ['film' => $films[0], 'films' => array_slice($films, 1)]);
     } else {
+
         $error = isset($_SESSION['error']) ? $_SESSION['error'] : '';
-        echo $twig->render('recherche_film.html.twig',[
+        echo $twig->render('recherche_film.html.twig', [
             'error' => $error
         ]);
     }
     unset($_SESSION['error']);
-}
-catch(Exception $e)
-{
+} catch (Exception $e) {
     $_SESSION['error'] = $e->getMessage();
     header('Location: ./');
 }
